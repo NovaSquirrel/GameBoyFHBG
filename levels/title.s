597e909085284bc8d30a7ca9b974b090eda15762
NovaSquirrel <bushytail.squirrel@gmail.com>
2018-07-27 21:21:53 -0400

    Better title screen

---
+++ levels/title.s
@@ -0,0 +1,15 @@
+title:
+  db LVL_V_CHECKER|$8, $a
+  db LVL_H_CHECKER|$a, $90
+  db LVL_H_DIRT|$7, $80
+  db LVL_SPRING,  $77
+  db LVL_FLOWER,  $62
+  db LVL_H_GRASS|$1, $60
+  db LVL_H_FENCE|$3, $73
+  db LVL_RECT|LVL_WATER,   $88, $1
+  db LVL_H_DIRT|$2, $70
+  db LVL_V_CHECKER|$2, $20
+  db LVL_V_CHECKER|$2, $26
+  db LVL_H_CHECKER|$4, $21
+  db LVL_H_CHECKER|$4, $41
+  db LVL_DONE, $67
